00ac26b3dc6dd26b3ca0b9b67b62a077ee48aa74
Ravi <ravkchauhan1aip@gmail.com>
2023-04-22 23:57:01 +0530

    update

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ const resultEl = document.getElementById('result')
 const lengthEl = document.getElementById('length')
 const uppercaseEl = document.getElementById('uppercase')
 const lowercaseEl = document.getElementById('lowercase')
+
+const numbersEl = document.getElementById('numbers')
+const symbolsEl = document.getElementById('symbols')
