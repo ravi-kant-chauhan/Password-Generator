76fc7571c00e3a99c608487b44ed7d843ef089b2
Ravi <ravkchauhan1aip@gmail.com>
2023-04-25 23:42:17 +0530

    update

--- script.js
+++ script.js
@@ -22,3 +22,8 @@ clipboardEl.addEventListener('click', () => {
   navigator.clipboard.writeText(password);
     alert('Password copied to clipboard!')
 })
+
+generateEl.addEventListener('click', () => {
+    const length = +lengthEl.value
+    const hasLower = lowercaseEl.checked
+})
