e7ed56e16158b430fe415dae8aaa7a96c561ba6f
Ravi <ravkchauhan1aip@gmail.com>
2023-04-26 23:52:39 +0530

    update

--- script.js
+++ script.js
@@ -39,4 +39,8 @@ function generatePassword(lower, upper, number, symbol, length) {
   let generatedPassword = ''
   const typesCount = lower + upper + number + symbol
   
+
+  const finalPassword = generatedPassword.slice(0, length)
+
+  return finalPassword
 }
