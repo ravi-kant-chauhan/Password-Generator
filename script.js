abe90faf40dfd19edbf369bfc674f7f785962838
Ravi <ravkchauhan1aip@gmail.com>
2023-04-26 23:53:15 +0530

    update

--- script.js
+++ script.js
@@ -36,14 +36,21 @@ generateEl.addEventListener('click', () => {
 
 
 function generatePassword(lower, upper, number, symbol, length) {
-  let generatedPassword = ''
-  const typesCount = lower + upper + number + symbol
-  const typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0])
-  
-  if(typesCount === 0) {
-      return ''
-  }
-  const finalPassword = generatedPassword.slice(0, length)
+    let generatedPassword = ''
+    const typesCount = lower + upper + number + symbol
+    const typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0])
+    
+    if(typesCount === 0) {
+        return ''
+    }
 
-  return finalPassword
+    for(let i = 0; i < length; i += typesCount) {
+        typesArr.forEach(type => {
+            generatedPassword += randomFunc[funcName]()
+        })
+    }
+
+    const finalPassword = generatedPassword.slice(0, length)
+
+    return finalPassword
 }
