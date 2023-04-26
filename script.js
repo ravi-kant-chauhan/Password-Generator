bd85084a72762596c29d3778c090be8872e9afd7
Ravi <ravkchauhan1aip@gmail.com>
2023-04-26 23:54:06 +0530

    update

--- script.js
+++ script.js
@@ -44,6 +44,9 @@ function generatePassword(lower, upper, number, symbol, length) {
         return ''
     }
 
+    function getRandomLower() {
+      return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+  }
     for(let i = 0; i < length; i += typesCount) {
         typesArr.forEach(type => {
             const funcName = Object.keys(type)[0]
