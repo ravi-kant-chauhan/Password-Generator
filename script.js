adf82e77da570a2e5b9e4de7b493717b9c5694d9
Ravi <ravkchauhan1aip@gmail.com>
2023-04-27 23:36:25 +0530

    update

--- script.js
+++ script.js
@@ -63,3 +63,8 @@ function getRandomLower() {
 function getRandomUpper() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
 }
+
+
+function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
+}
